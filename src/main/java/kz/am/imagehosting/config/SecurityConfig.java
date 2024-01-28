7bc203553e5ef2e76721cc14e235a10b638bdb20
CorvoLarida <bktowork@protonmail.com>
2024-01-28 21:29:18 +0600

    010

--- src/main/java/kz/am/imagehosting/config/SecurityConfig.java
+++ src/main/java/kz/am/imagehosting/config/SecurityConfig.java
@@ -2,10 +2,16 @@ package kz.am.imagehosting.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @Configuration
@@ -18,21 +24,39 @@ public class SecurityConfig {
                 .password("user11")
                 .roles("USER")
                 .build();
-        UserDetails user2= User.withDefaultPasswordEncoder()
+        UserDetails user2 = User.withDefaultPasswordEncoder()
                 .username("user22")
                 .password("user22")
                 .roles("USER")
                 .build();
-        UserDetails user3= User.withDefaultPasswordEncoder()
+        UserDetails user3 = User.withDefaultPasswordEncoder()
                 .username("user33")
                 .password("user33")
                 .roles("USER")
                 .build();
-        UserDetails admin= User.withDefaultPasswordEncoder()
+        UserDetails admin = User.withDefaultPasswordEncoder()
                 .username("admin")
                 .password("admin")
                 .roles("ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user1, user2, user3, admin);
+    }
+    @Bean
+    static RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
+        hierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER\n");
+        return hierarchy;
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers("/").permitAll()
+                    .requestMatchers("/posts/**").hasRole("USER")
+                    .requestMatchers("/collections/**").hasRole("ADMIN")
+                    .anyRequest().authenticated()
+            )
+            .formLogin(withDefaults());
+        return http.build();
     }
 }
